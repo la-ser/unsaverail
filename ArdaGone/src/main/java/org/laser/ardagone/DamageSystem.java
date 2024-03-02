c4ba3dc47fea2fb6233aec839e67eb71b89e86cf
la-ser <77540680+la-ser@users.noreply.github.com>
2024-03-02 09:54:15 +0100

    new

--- ArdaGone/src/main/java/org/laser/ardagone/DamageSystem.java
+++ ArdaGone/src/main/java/org/laser/ardagone/DamageSystem.java
@@ -2,32 +2,36 @@ package org.laser.ardagone;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class DamageSystem {
+public class DamageSystem implements Listener {
     private final ArdaGone plugin;
     private final Characters characters;
 
     public DamageSystem(ArdaGone plugin, Characters characters) {
         this.plugin = plugin;
         this.characters = characters;
+
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
     public void onPlayerAttack(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player) {
             Player player = (Player) event.getDamager();
+            Player targetPlayer = (Player) event.getEntity();
             int playerCharID = characters.getCharacter(player);
             ItemStack weapon = player.getInventory().getItemInMainHand();
 
-            /*if (weapon.getType().equals(Material.WOODEN_SWORD) && weapon.getItemMeta().getCustomModelData() == 1) {
-                player.sendMessage("You have attacked as Henry!");
-            }*/
             if (playerCharID == 0) {
                 player.sendMessage("You have attacked as Henry!");
+                targetPlayer.sendMessage("you have been attacked by henry");
             } else if (playerCharID == 1) {
                 player.sendMessage("You have attacked as Robin!");
+            } else if (playerCharID == 2) {
+                player.sendMessage("You have attacked as Thorn!");
             }
         }
     }
