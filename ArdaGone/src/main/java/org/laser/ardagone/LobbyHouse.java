3ca24b3d457d152f8e0551da3efe7833beb16a02
la-ser <77540680+la-ser@users.noreply.github.com>
2024-03-03 14:36:17 +0100

    Changed made by laser

--- ArdaGone/src/main/java/org/laser/ardagone/LobbyHouse.java
+++ ArdaGone/src/main/java/org/laser/ardagone/LobbyHouse.java
@@ -4,7 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Structure;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,20 +13,18 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.util.BlockVector;
+import org.bukkit.util.Vector;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class LobbyHouse implements Listener {
     private final Plugin plugin;
-    private final Map<Player, Location> playerHouses;
     private final FileConfiguration config;
 
     public LobbyHouse(Plugin plugin) {
         this.plugin = plugin;
-        this.playerHouses = new HashMap<>();
         this.config = plugin.getConfig();
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
@@ -34,11 +33,6 @@ public class LobbyHouse implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         generateHouse(player);
-        // Teleport player to their house
-        Location houseLocation = playerHouses.get(player);
-        if (houseLocation != null) {
-            player.teleport(houseLocation);
-        }
     }
 
     @EventHandler
@@ -52,54 +46,90 @@ public class LobbyHouse implements Listener {
         Location houseLocation = findFreeLocation(world);
         if (houseLocation != null) {
             // Generate the structure block at the house location
-            houseLocation.getBlock().setType(Material.STRUCTURE_BLOCK);
-            Location redstoneLocation = new Location(world, houseLocation.getX(), houseLocation.getY() - 1, houseLocation.getZ());
-            redstoneLocation.getBlock().setType(Material.REDSTONE_BLOCK);
-            playerHouses.put(player, houseLocation);
+            Location structureBlockLocation = new Location(world, houseLocation.getX(), houseLocation.getY() + 1, houseLocation.getZ());
+            structureBlockLocation.getBlock().setType(Material.STRUCTURE_BLOCK);
+            BlockState blockState = structureBlockLocation.getBlock().getState();
+            if (blockState instanceof Structure) {
+                Structure structureBlock = (Structure) blockState;
+                structureBlock.setStructureName("factory_1");
+                structureBlock.setRelativePosition(new BlockVector(0, 0, 0));
+                structureBlock.update();
+            }
+
+            houseLocation.getBlock().setType(Material.AIR);
+            houseLocation.getBlock().setType(Material.REDSTONE_BLOCK);
+
+            Location playerHouseLocation = houseLocation.clone().add(new Vector(11, 2, 11.5));
+            playerHouseLocation.setYaw(130);
+            playerHouseLocation.setPitch(0);
+            player.teleport(playerHouseLocation);
 
             // Save house location to config
-            config.set("houses." + player.getUniqueId().toString(), houseLocation);
-            try {
-                config.save(new File(plugin.getDataFolder(), "houses.yml"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            String path = "houses." + world.getName() + "." + houseLocation.getBlockX() + "." + houseLocation.getBlockY() + "." + houseLocation.getBlockZ();
+            config.set(path, player.getUniqueId().toString());
+            saveConfig();
         }
     }
 
     private void deleteHouse(Player player) {
-        Location houseLocation = playerHouses.remove(player);
-        if (houseLocation != null) {
-            // Remove the structure block at the house location
-            houseLocation.getBlock().setType(Material.AIR);
+        if (config.isConfigurationSection("houses")) {
+            for (String worldName : config.getConfigurationSection("houses").getKeys(false)) {
+                World world = Bukkit.getWorld(worldName);
+                if (world != null && config.isConfigurationSection("houses." + worldName)) {
+                    for (String xStr : config.getConfigurationSection("houses." + worldName).getKeys(false)) {
+                        int x = Integer.parseInt(xStr);
+                        if (config.isConfigurationSection("houses." + worldName + "." + xStr)) {
+                            for (String yStr : config.getConfigurationSection("houses." + worldName + "." + xStr).getKeys(false)) {
+                                int y = Integer.parseInt(yStr);
+                                if (config.isConfigurationSection("houses." + worldName + "." + xStr + "." + yStr)) {
+                                    for (String zStr : config.getConfigurationSection("houses." + worldName + "." + xStr + "." + yStr).getKeys(false)) {
+                                        int z = Integer.parseInt(zStr);
+                                        String playerId = config.getString("houses." + worldName + "." + xStr + "." + yStr + "." + zStr);
+                                        if (playerId.equals(player.getUniqueId().toString())) {
+                                            Location houseLocation = new Location(world, x, y, z);
+                                            Location structureBlockLocation = new Location(world, houseLocation.getX(), houseLocation.getY() + 1, houseLocation.getZ());
+                                            structureBlockLocation.getBlock().setType(Material.STRUCTURE_BLOCK);
+                                            BlockState blockState = structureBlockLocation.getBlock().getState();
+                                            if (blockState instanceof Structure) {
+                                                Structure structureBlock = (Structure) blockState;
+                                                structureBlock.setStructureName("factory_0");
+                                                structureBlock.setRelativePosition(new BlockVector(0, 0, 0));
+                                                structureBlock.update();
+                                            }
 
-            // Place a gold block at the house location
-            Location goldLocation = new Location(houseLocation.getWorld(), houseLocation.getX(), houseLocation.getY() - 1, houseLocation.getZ());
-            goldLocation.getBlock().setType(Material.GOLD_BLOCK);
-
-            // Remove player's house from config
-            config.set("houses." + player.getUniqueId().toString(), null);
-            try {
-                config.save(new File(plugin.getDataFolder(), "houses.yml"));
-            } catch (IOException e) {
-                e.printStackTrace();
+                                            houseLocation.getBlock().setType(Material.AIR);
+                                            houseLocation.getBlock().setType(Material.REDSTONE_BLOCK);
+                                            config.set("houses." + worldName + "." + xStr + "." + yStr + "." + zStr, null);
+                                            saveConfig();
+                                            return;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
     private Location findFreeLocation(World world) {
-        // Start at coordinates (50, 56, 50)
-        Location location = new Location(world, 50, 56, 50);
-        Block block = location.getBlock();
-
-        // Check if there's already a house at the current location
-        while (block.getType() == Material.REDSTONE_BLOCK || block.getType() == Material.STRUCTURE_BLOCK) {
-            // Move 25 blocks in the positive z direction
-            location.add(0, 0, 25);
-            block = location.getBlock();
+        for (int x = 50; x < 100; x += 25) {
+            for (int z = 50; z < 100; z += 25) {
+                Location location = new Location(world, x, 56, z);
+                if (!config.contains("houses." + world.getName() + "." + x + "." + 56 + "." + z)) {
+                    return location;
+                }
+            }
         }
+        return null;
+    }
 
-        // Return the location if it's free
-        return location;
+    private void saveConfig() {
+        try {
+            config.save(new File(plugin.getDataFolder(), "houses.yml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
